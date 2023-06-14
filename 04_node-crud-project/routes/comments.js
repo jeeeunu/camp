5d4459c4e82ef810fc85a55a3f8803f36d8a4c1e
ug <dbwldms25@gmail.com>
2023-06-14 23:00:34 +0900

    feat : PUT/댓글 수정하기

--- 04_node-crud-project/routes/comments.js
+++ 04_node-crud-project/routes/comments.js
@@ -7,7 +7,7 @@ const clientComments = [
   },
   {
     user: "댓글 작성자2",
-    password: "1234",
+    password: "12ㄴㄴ34",
     content: "안녕하세요 댓글입니다.22"
   },
 ];
@@ -15,7 +15,6 @@ const clientComments = [
 //-- 클라이언트 : 댓글 수정 --//
 const clientEditComments = {
   password: "1234",
-  content: "수정된 댓글입니다.",
 }
 
 //-- 클라이언트 : 댓글 삭제 --//
@@ -27,60 +26,23 @@ const cliendDeleteComments = {
 const express = require('express');
 const router = express.Router();
 const commentSchema = require('../schemas/comment-shema');
-const postSchema = require('../schemas/post-shema')
+// const postSchema = require('../schemas/post-shema')
 const mongoose = require('mongoose');
 
 
 // POST : 댓글 등록하기
-// router.post('/:postID', async (req, res) => {
-//   const { postID } = req.params;
-//   try {
-
-//     const existingPost = await postSchema.findById(postID);
-//     if (!existingPost) {
-//       return;
-//     }
-//     req.body = clientComments;
-
-//     await Promise.all(clientComments.map(async (comment) => {
-//       const newComment = new commentSchema({
-//         ...comment,
-//         post: postID,
-//         createdAt: new Date(),
-//       });
-//       await newComment.save();
-//     }));
-
-//     res.status(200).json({ message: '댓글이 작성되었습니다.' });
-
-//   } catch (error) {
-//     switch (error.name) {
-//       case "CastError":
-//         res.status(404).json({
-//           "message": "그런 게시물은 없습니다만",
-//           "error": error
-//         });
-//         break;
-//       default:
-//         res.status(400).json({
-//           "message": "게시글 조회에 실패하였습니다.",
-//           "error": error
-//         });
-//         break;
-//     }
-//   }
-// });
-
 router.post('/:postID', async (req, res) => {
   const { postID } = req.params;
   req.body = clientComments;
 
+  // postID 유효성 검사
+  if (!mongoose.Types.ObjectId.isValid(postID)) {
+    res.status(404).json({ message: '유효하지 않은 게시물 ID입니다.' });
+    return;
+  }
+
   try {
-    // postID 유효성 검사
-    if (!mongoose.Types.ObjectId.isValid(postID)) {
-      res.status(404).json({ message: '유효하지 않은 게시물 ID입니다.' });
-      return;
-    }
+
 
     // req.body 유효성 검사
     if (!req.body.length) {
@@ -101,6 +63,51 @@ router.post('/:postID', async (req, res) => {
     res.status(200).json({ message: '댓글이 작성되었습니다.' });
   } catch (error) {
     res.status(400).json({ message: '게시글 조회에 실패하였습니다.', error: error });
+  }
+});
+
+// GET : 게시물에 맞는 댓글 데이터 가져오기
+router.get('/:postID', async (req, res) => {
+  const { postID } = req.params;
+  try {
+    const resultDatas = await commentSchema.find({ post: postID }, '_id user content'); // 뒤의 '_id .. ' 는 mongoDB 문법으로 특정 필드만 반환하도록 지정함
+
+    // 해당하는 postID만 가져오기
+    res.json(resultDatas);
+  } catch (error) {
+    res.status(400).json({ "message": "Error" });
+  }
+});
+
+// PUT : 댓글 수정하기
+router.put('/:commentID', async (req, res) => {
+  const { commentID } = req.params;
+  req.body = clientEditComments;
+
+  // commentID 유효성 검사
+  if (!mongoose.Types.ObjectId.isValid(commentID)) {
+    res.status(404).json({ "message": "해당하는 데이터가 없습니다." })
+    return;
+  }
+
+  try {
+    const commentData = await commentSchema.findById(commentID);
+
+
+    if (!req.body.comment) {
+      res.status(400).json({ "message": "댓글내용을  입력해주세요." })
+      return;
+    }
+
+    if (commentData.password === req.body.password) {
+      await commentData.updateOne({ $set: req.body })
+      res.status(200).json({ "message": "댓글 수정 완료" })
+    } else {
+      res.status(400).json({ "message": "비번 아님" })
+    }
+
+  } catch (error) {
+    res.status(400).json({ "message": "오류", "error": error })
   }
 });
 
