6df81bcf8eee2e65cb9193c4717bf951541c0fa7
ug <dbwldms25@gmail.com>
2023-06-21 11:46:42 +0900

    feat : 게시물 수정,삭제

--- 05_sequelize_init/routes/post.route.js
+++ 05_sequelize_init/routes/post.route.js
@@ -2,6 +2,7 @@
 
 const express = require("express");
 const { Posts } = require("../models");
+const { Op } = require('sequelize');
 const router = express.Router();
 
 
@@ -36,6 +37,70 @@ router.get("/posts/:postId", async (req, res) => {
 
   res.status(200).json({ data: post });
 })
+
+
+// 게시글 수정 API
+router.put("/posts/:postId", async (req, res) => {
+  const { postId } = req.params;
+  const { title, content, password } = req.body;
+
+  const post = await Posts.findOne({
+    where: { postId: postId },
+  });
+
+  if (!post) {
+    return res.status(404).json({
+      message: "게시글이 존재하지 않습니다.",
+    })
+  } else if (post.password !== password) {
+    return res.status(401).json({
+      message: "게시글의 비밀번호와 전달받은 비밀번호가 일치하지않슴다"
+    })
+  }
+
+  // 게시글을 수정
+  await Posts.update(
+    { title, content }, // 수정할 컬럼 및 데이터
+    {
+      where: {
+        [Op.and]: [{ postId }, { password }] // 게시글의 비밀번호와 postId가 일치할때 수정한다
+      },
+    } // 어떤 데이터를 수정할 지 작성
+  )
+  return res.status(200).json({
+    message: "게시글이 수정되었습니다."
+  })
+});
+
+// 게시글 삭제 API
+router.delete("/posts/:postId", async (req, res) => {
+  const { postId } = req.params;
+  const { password } = req.body;
+
+  const post = await Posts.findOne({
+    where: { postId: postId },
+  });
+
+  if (!post) {
+    return res.status(404).json({
+      message: "게시글이 존재하지 않습니다.",
+    })
+  } else if (post.password !== password) {
+    return res.status(401).json({
+      message: "게시글의 비밀번호와 전달받은 비밀번호가 일치하지않슴다"
+    })
+  }
+
+  // 게시글을 삭제
+  await Posts.destroy({
+    where: {
+      [Op.and]: [{ postId }, { password }] // 게시글의 비밀번호와 postId가 일치할때 삭제한다
+    },
+  })
+  return res.status(200).json({
+    message: "게시글이 삭제되었습니다."
+  })
+});
 
 
 
